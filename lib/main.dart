2720a1c7c4f90ec7b9144d849983d6b9e2e9212f
deathgaser12 <thalysonm@outlook.com>
2021-05-25 11:56:11 -0300

    estruturas de pastas criada, libs importadas

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Estética App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        BouncingScrollWrapper.builder(
+          context,
+          widget!,
+        ),
+        maxWidth: 1200,
+        minWidth: 450,
+        defaultScale: true,
+        breakpoints: [
+          ResponsiveBreakpoint.autoScale(450, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          ResponsiveBreakpoint.autoScale(1200, name: DESKTOP),
+          ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+        ],
+      ),
+      home: Container(),
+    );
+  }
+}
